ab301ecfa86148b825af51a1b262fab761938d59
Martin Alexander Flores Torres <martinflorestorres21@gmail.com>
2023-09-10 16:28:35 -0500

    mejoras

--- src/components/Galeria.tsx
+++ src/components/Galeria.tsx
@@ -2,54 +2,120 @@ const Galeria = () => {
   return (
     <div className='galeria'>
       <div className='galeria__item'>
-        <img loading='lazy' src='/img/galeria/foto-1.jpg' alt='imagen 1' />
+        <picture>
+          <source srcSet='/img/galeria/foto-1.webp' type='image/webp' />
+          <source srcSet='/img/galeria/foto-1.jpg' type='image/jpeg' />
+          <img src='/img/galeria/foto-1.jpg' alt='imagen' />
+        </picture>
       </div>
       <div className='galeria__item'>
-        <img loading='lazy' src='/img/galeria/foto-2.jpg' alt='imagen 2' />
-      </div>
-
-      <div className='galeria__item'>
-        <img loading='lazy' src='/img/galeria/foto-3.jpg' alt='imagen 3' />
-      </div>
-
-      <div className='galeria__item'>
-        <img loading='lazy' src='/img/galeria/foto-4.jpg' alt='imagen 4' />
-      </div>
-
-      <div className='galeria__item'>
-        <img loading='lazy' src='/img/galeria/foto-5.jpg' alt='imagen 5' />
+        <picture>
+          <source srcSet='/img/galeria/foto-2.webp' type='image/webp' />
+          <source srcSet='/img/galeria/foto-2.jpg' type='image/jpeg' />
+          <img src='/img/galeria/foto-2.jpg' alt='imagen' />
+        </picture>
       </div>
 
       <div className='galeria__item'>
-        <img loading='lazy' src='/img/galeria/foto-6.jpg' alt='imagen 6' />
+        <picture>
+          <source srcSet='/img/galeria/foto-3.webp' type='image/webp' />
+          <source srcSet='/img/galeria/foto-3.jpg' type='image/jpeg' />
+          <img src='/img/galeria/foto-3.jpg' alt='imagen' />
+        </picture>
       </div>
 
       <div className='galeria__item'>
-        <img loading='lazy' src='/img/galeria/foto-7.jpg' alt='imagen 7' />
+        <picture>
+          <source srcSet='/img/galeria/foto-4.webp' type='image/webp' />
+          <source srcSet='/img/galeria/foto-4.jpg' type='image/jpeg' />
+          <img src='/img/galeria/foto-4.jpg' alt='imagen' />
+        </picture>
       </div>
 
       <div className='galeria__item'>
-        <img loading='lazy' src='/img/galeria/foto-8.jpg' alt='imagen 8' />
+        <picture>
+          <source srcSet='/img/galeria/foto-5.webp' type='image/webp' />
+          <source srcSet='/img/galeria/foto-5.jpg' type='image/jpeg' />
+          <img src='/img/galeria/foto-5.jpg' alt='imagen' />
+        </picture>
       </div>
 
       <div className='galeria__item'>
-        <img loading='lazy' src='/img/galeria/foto-9.jpg' alt='imagen 9' />
+        <picture>
+          <source srcSet='/img/galeria/foto-6.webp' type='image/webp' />
+          <source srcSet='/img/galeria/foto-6.jpg' type='image/jpeg' />
+          <img src='/img/galeria/foto-6.jpg' alt='imagen' />
+        </picture>
       </div>
 
       <div className='galeria__item'>
-        <img loading='lazy' src='/img/galeria/foto-10.jpg' alt='imagen 10' />
+        <picture>
+          <source srcSet='/img/galeria/foto-7.webp' type='image/webp' />
+          <source srcSet='/img/galeria/foto-7.jpg' type='image/jpeg' />
+          <img src='/img/galeria/foto-7.jpg' alt='imagen' />
+        </picture>
       </div>
 
       <div className='galeria__item'>
-        <img loading='lazy' src='/img/galeria/foto-11.jpg' alt='imagen 11' />
+        <picture>
+          <source srcSet='/img/galeria/foto-8.webp' type='image/webp' />
+          <source srcSet='/img/galeria/foto-8.jpg' type='image/jpeg' />
+          <img src='/img/galeria/foto-8.jpg' alt='imagen' />
+        </picture>
       </div>
 
       <div className='galeria__item'>
-        <img loading='lazy' src='/img/galeria/foto-12.jpg' alt='imagen 12' />
+        <picture>
+          <source srcSet='/img/galeria/foto-9.webp' type='image/webp' />
+          <source srcSet='/img/galeria/foto-9.jpg' type='image/jpeg' />
+          <img src='/img/galeria/foto-9.jpg' alt='imagen' />
+        </picture>
       </div>
 
       <div className='galeria__item'>
-        <img loading='lazy' src='/img/galeria/foto-13.jpg' alt='imagen 13' />
+        <picture>
+          <source srcSet='/img/galeria/foto-10.webp' type='image/webp' />
+          <source srcSet='/img/galeria/foto-10.jpg' type='image/jpeg' />
+          <img src='/img/galeria/foto-10.jpg' alt='imagen' />
+        </picture>
+      </div>
+
+      <div className='galeria__item'>
+        <picture>
+          <source srcSet='/img/galeria/foto-11.webp' type='image/webp' />
+          <source srcSet='/img/galeria/foto-11.jpg' type='image/jpeg' />
+          <img src='/img/galeria/foto-11.jpg' alt='imagen' />
+        </picture>
+      </div>
+
+      <div className='galeria__item'>
+        <picture>
+          <source srcSet='/img/galeria/foto-12.webp' type='image/webp' />
+          <source srcSet='/img/galeria/foto-12.jpg' type='image/jpeg' />
+          <img src='/img/galeria/foto-12.jpg' alt='imagen' />
+        </picture>
+      </div>
+
+      <div className='galeria__item'>
+        <picture>
+          <source srcSet='/img/galeria/foto-13.webp' type='image/webp' />
+          <source srcSet='/img/galeria/foto-13.jpg' type='image/jpeg' />
+          <img src='/img/galeria/foto-13.jpg' alt='imagen' />
+        </picture>
+      </div>
+      <div className='galeria__item'>
+        <picture>
+          <source srcSet='/img/galeria/foto-14.webp' type='image/webp' />
+          <source srcSet='/img/galeria/foto-14.jpg' type='image/jpeg' />
+          <img src='/img/galeria/foto-14.jpg' alt='imagen' />
+        </picture>
+      </div>
+      <div className='galeria__item'>
+        <picture>
+          <source srcSet='/img/galeria/foto-15.webp' type='image/webp' />
+          <source srcSet='/img/galeria/foto-15.jpg' type='image/jpeg' />
+          <img src='/img/galeria/foto-15.jpg' alt='imagen' />
+        </picture>
       </div>
     </div>
   )
